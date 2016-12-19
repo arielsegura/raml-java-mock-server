72a3ae1c559634b7a0201aead0887e4646ba24c0
Ariel <ariel.segura@yahoo.com>
2016-12-18 23:58:43 -0300

    test fixing

--- src/test/java/ramlmockserver/api/BuilderTest.java
+++ src/test/java/ramlmockserver/api/BuilderTest.java
@@ -16,15 +16,51 @@
 package ramlmockserver.api;
 
 import org.junit.Test;
+import ramlmockserver.api.requestmapping.RequestMapping;
+import ramlmockserver.api.requestmapping.RequestMappingBuilder;
+import ramlmockserver.internal.ResourceMap;
+import ramlmockserver.internal.UriParameter;
 
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
  * Created by Ariel on 18/12/2016.
  */
 public class BuilderTest {
+    String employeeId = "1234";
+    String invalidPhoneId = "invalidPhoneId";
+    String invalidEmployeeId = "invalidEmployeeId";
+    String phoneId = "1234556";
+
     @Test
-    public void implement(){
-        fail();
+    public void simpleScenario(){
+        String resourceName = "/v1/employees/{employeeId}/phones";
+        RequestMapping requestMapping =
+                RequestMappingBuilder.requestMappingBuilder().
+                        addResource(resourceName, RequestMappingBuilder.resourceConfig()
+                                .configureStatusCode(404, RequestMappingBuilder.uriParameter("employeeId", invalidEmployeeId))
+                                .configureStatusCode(200, RequestMappingBuilder.uriParameter("employeeId", employeeId))
+                                .build())
+                        .build();
+
+        Optional<ResourceMap> resource = requestMapping.getResource(resourceName);
+        assertTrue(resource.isPresent());
+        ResourceMap resourceMap = resource.get();
+
+        Optional<UriParameter[]> notFound = resourceMap.getStatusCode(404);
+        assertTrue(notFound.isPresent());
+
+        UriParameter[] uriParameters = notFound.get();
+        assertTrue(Arrays.stream(uriParameters).anyMatch(uriParameter -> uriParameter.getKey().equals("employeeId")));
+
+        Optional<UriParameter[]> ok = resourceMap.getStatusCode(404);
+        assertTrue(ok.isPresent());
+
+        uriParameters = ok.get();
+        assertTrue(Arrays.stream(uriParameters).anyMatch(uriParameter -> uriParameter.getKey().equals("employeeId")));
     }
 }
